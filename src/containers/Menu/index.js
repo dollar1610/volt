9436eedca53dcd8b4fab19172cca077cfd407267
dollar1610 <Dollar16.10.91@mail.ru>
2019-02-08 14:36:20 +0500

    added files

---
+++ src/containers/Menu/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Container, Nav, NavItem, Navbar, NavbarToggler, Collapse } from 'reactstrap';
+import { Link } from 'react-router';
+
+export default class Menu extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div className="menu bg-light">
+        <Container>
+          <Navbar color="light" light expand="md">
+            <Nav>
+              <NavItem className="logo">
+                <Link to="/">Invoice App</Link>
+              </NavItem>
+            </Nav>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="menu-items" navbar>
+                <NavItem>
+                  <Link to="/invoices">Invoices</Link>
+                </NavItem>
+                <NavItem className="menu-items__item">
+                  <Link to="/products">Products</Link>
+                </NavItem>
+                <NavItem className="menu-items__item">
+                  <Link to="/customers">Customers</Link>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Navbar>
+        </Container>
+      </div>
+    )
+  }
+}
